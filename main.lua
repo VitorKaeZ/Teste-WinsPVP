1ef8a6de800fafc1c06ac66e53a5047518d9a0c8
VitorKaeZ <vitorcaetanoz@outlook.com>
2023-09-20 00:11:39 -0300

    test

--- main.lua
+++ main.lua
@@ -8,25 +8,42 @@ local teams = {
 }
 
 local function createTeams(groups)
-    for gName, group in pairs(groups) do
-        for index, player in pairs(group.players) do
-            if #teams['blue'].players < 5 then
-                local playerData = {
-                    id = player.id,
-                    name = player.name,
-                    group = gName
-                }
-                table.insert(teams['blue'].players, playerData)
-            elseif #teams['red'].players < 5 then
-                local playerData = {
-                    id = player.id,
-                    name = player.name,
-                    group = gName
-                }
-                table.insert(teams['red'].players, playerData)
+    for index, team in pairs(teams) do
+        local players = #teams[index].players < 5
+        while players do
+            print(players)
+            for gName, group in ipairs(groups) do
+                if 5 - #teams[index].players >= #group.players then
+                    for _, player in ipairs(group.players) do
+                        local playerData = {
+                            id = player.id,
+                            name = player.name,
+                            group = gName
+                        }
+                        table.insert(teams[index].players, playerData)
+                        table.remove(groups[gName].players, 1)
+                    end
+                end
             end
+            players = #teams[index].players < 5
         end
     end
+
+    -- for gName, group in pairs(groups) do
+    --     for i, team in pairs(teams) do
+    --         if 5 - #team.players >= #group.players then
+    --             for index, player in pairs(group.players) do
+    --                 local playerData = {
+    --                     id = player.id,
+    --                     name = player.name,
+    --                     group = gName
+    --                 }
+    --                 table.insert(teams[i].players, playerData)
+    --                 table.remove(group.players, index)
+    --             end
+    --         end
+    --     end
+    -- end
 end
 
 
